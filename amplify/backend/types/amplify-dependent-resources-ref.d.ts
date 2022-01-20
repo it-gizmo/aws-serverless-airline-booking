85d9542ecd8b2a2e6b6e26038d8084ea2586a2e2
Jakub Szepielak <jakub.szepielak@pwc.com>
2022-01-20 09:32:17 +0100

    working episode 0

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "awsserverlessairlinefee9fea8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
